e9e9be0f800eba0ce321cc3c6f62d6b650f8f0bf
Pedro Marcelo <pedrocarvalhomarcelo@hotmail.com>
2020-06-22 20:31:10 -0300

    file system added

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import mainLex
+
+
+def get_file_content(path):
+    file = open(path)
+    content = file.read()
+    return str(content), 1
+
+
+def get_file_by_name(name):
+    dash = r'\\'
+    ext = '.201'
+    path = os.getcwdb()
+    path_convert = os.path.abspath(name) + '.201'
+    file = open(path_convert.replace(r"b'",  ''))
+    content = file.read()
+    return content
+
+
+def main():
+    user_path = input("enter your file: ")
+    if os.path.isabs(user_path):
+        try:
+            get_file_content(user_path)
+        except:
+            print("file doesn't exist")
+    else:
+        try:
+            get_file_by_name(user_path)
+        except:
+            print("wrong name of file or file doesn't exist")
+
+
+if __name__ == "__main__":
+    main()
--- mainLex.py
+++ mainLex.py
@@ -1,115 +1,114 @@
 import lex as lex
 import math
-#tolkens/atomos e codigo
+import os
+# tolkens/atomos e codigo
+
 Dic_Reserved_Atom_Cod = {
-'Bool': '310' , 
-'End' : '318',
-'diferenteIgual' : '410',  
-'diferente' : '421', 
-'While' : '311', 
-'Return' : '319', 
- 'comentario' : '410', 
- 'porcento': '422', 
-'Break':"312", 
- 'False':"320" ,  
-'and' : "411",  
-'fechaparentese' : "423", 
-'Void' : "313", 
- 'Program' : "321", 
- 'abreparentese' : "412",  
-'vezes' : "424",  
-'Char' : "314", 
- 'Float' : "322", 
- 'dividido' : "413", 
- 'virgula': "425",  
-'True' : "315",  
- 'Int' : "323", 
- 'pontoevirgula' : "414",  
-'fechachave' : "426",  
-'Else' : "316",  
-'If' : "324", 
- 'abrechave' : "415",  
-'ou' : "427", 
-'String' : "317",  
-'Begin' : "325",  
-'abrecolchete' : "416",  
-'fechacolchete' : "428", 
-'mais' : "417", 
- 'menorigual' : "418",
- 'maiorigual' : "420",
- 'igualigual' : "430", 
- 'menor' : "429", 
-'igual' : "419",  
-'maior' : "431", 
-'menos' : "432",
-'character' : "510",
-'constantString' : "511",
-'floatNum' : "512",
-'intergerNum' : "515",
-'identifier' : "514",
-'function' : "513"
+    'Bool': '310',
+    'End': '318',
+    'diferenteIgual': '410',
+    'diferente': '421',
+    'While': '311',
+    'Return': '319',
+    'comentario': '410',
+    'porcento': '422',
+    'Break': "312",
+    'False': "320",
+    'and': "411",
+    'fechaparentese': "423",
+    'Void': "313",
+    'Program': "321",
+    'abreparentese': "412",
+    'vezes': "424",
+    'Char': "314",
+    'Float': "322",
+    'dividido': "413",
+    'virgula': "425",
+    'True': "315",
+    'Int': "323",
+    'pontoevirgula': "414",
+    'fechachave': "426",
+    'Else': "316",
+    'If': "324",
+    'abrechave': "415",
+    'ou': "427",
+    'String': "317",
+    'Begin': "325",
+    'abrecolchete': "416",
+    'fechacolchete': "428",
+    'mais': "417",
+    'menorigual': "418",
+    'maiorigual': "420",
+    'igualigual': "430",
+    'menor': "429",
+    'igual': "419",
+    'maior': "431",
+    'menos': "432",
+    'character': "510",
+    'constantString': "511",
+    'floatNum': "512",
+    'intergerNum': "515",
+    'identifier': "514",
+    'function': "513"
 
 }
 
 Dic_not_Reserved_Atom = []
 
-
-
 tokens = (
-'Bool', 
-'End',
-'diferenteIgual',  
-'diferente', 
-'While', 
-'Return', 
- 'comentario', 
- 'porcento', 
-'Break', 
- 'False',  
-'and',  
-'fechaparentese', 
-'Void', 
- 'Program', 
- 'abreparentese',  
-'vezes',  
-'Char', 
- 'Float', 
- 'dividido', 
- 'virgula',  
-'True', 
- 'Int', 
- 'pontoevirgula',  
-'fechachave',  
-'Else',  
-'If', 
- 'abrechave',  
-'ou', 
-'String',  
-'Begin',  
-'abrecolchete',  
-'fechacolchete', 
-'mais', 
- 'menorigual',
- 'maiorigual',
- 'igualigual', 
- 'menor', 
-'igual',  
-'maior', 
-'menos',
-'character',
-'constantString',
-'floatNum',
-'intergerNum',
-'identifier',
-'function'
+    'Bool',
+    'End',
+    'diferenteIgual',
+    'diferente',
+    'While',
+    'Return',
+    'comentario',
+    'porcento',
+    'Break',
+    'False',
+    'and',
+    'fechaparentese',
+    'Void',
+    'Program',
+    'abreparentese',
+    'vezes',
+    'Char',
+    'Float',
+    'dividido',
+    'virgula',
+    'True',
+    'Int',
+    'pontoevirgula',
+    'fechachave',
+    'Else',
+    'If',
+    'abrechave',
+    'ou',
+    'String',
+    'Begin',
+    'abrecolchete',
+    'fechacolchete',
+    'mais',
+    'menorigual',
+    'maiorigual',
+    'igualigual',
+    'menor',
+    'igual',
+    'maior',
+    'menos',
+    'character',
+    'constantString',
+    'floatNum',
+    'intergerNum',
+    'identifier',
+    'function'
 
 )
 
-
 # Tokens
-t_Bool =  r'Bool'
+t_Bool = r'Bool'
 t_End = r'End'
-t_diferenteIgual = r'!=' 
+t_diferenteIgual = r'!='
 t_diferente = r'!'
 t_While = r'While'
 t_Return = r'Return'
@@ -117,28 +116,28 @@ t_comentario = r'\#'
 t_porcento = r'%'
 t_Break = r'Break'
 t_False = r'False'
-t_and =  r'&'
+t_and = r'&'
 t_fechaparentese = r'\)'
 t_Void = r'Void'
 t_Program = r'Program'
-t_abreparentese  =  r'\('
-t_vezes =  r'\*'
+t_abreparentese = r'\('
+t_vezes = r'\*'
 t_Char = r'Char'
 t_Float = r'Float'
 t_dividido = r'/'
 t_virgula = r','
 t_True = r'True'
 t_Int = r'Int'
-t_pontoevirgula = r';' 
-t_fechachave =  r'\]'
-t_Else =  r'Else'
+t_pontoevirgula = r';'
+t_fechachave = r'\]'
+t_Else = r'Else'
 t_If = r'If'
 t_abrechave = r'\['
 t_ou = r'\|'
 t_String = r'String'
-t_Begin = r'Begin' 
+t_Begin = r'Begin'
 t_abrecolchete = r'\{'
-t_fechacolchete =r'\}'
+t_fechacolchete = r'\}'
 t_mais = r'\+'
 t_menorigual = r'<='
 t_maiorigual = r'>='
@@ -147,17 +146,17 @@ t_menor = r'<'
 t_igual = r'='
 t_maior = r'>'
 t_menos = r'-'
-#regex derivados
+# regex derivados
 letra = r'[a-z]'
 digito = r'[0-9]'
 t_character = r'\'' + letra + r'\''
 branco = r'\s'
 
-t_constantString = r'\"' + r'[a-z\s0-9.&]*'  + r'\"'
+t_constantString = r'\"' + r'[a-z\s0-9.&]*' + r'\"'
 
 decimalDigit = r'[0-9]+'
-#expPart = r'e[-+\s]{1}' + decimalDigit
-t_floatNum = decimalDigit +r'\.' +decimalDigit + r'(e[-+]?[0-9]+)?' 
+# expPart = r'e[-+\s]{1}' + decimalDigit
+t_floatNum = decimalDigit + r'\.' + decimalDigit + r'(e[-+]?[0-9]+)?'
 
 t_intergerNum = decimalDigit
 
@@ -165,14 +164,9 @@ t_identifier = r'[a-z_][a-z_0-9]*'
 
 t_function = r'[a-z]\s|[a-z_][a-z_0-9]*([0-9]|[a-z])'
 
-
-
-
 # Function 513
-    #< Function > ::= < letra > | < Identifier > <letra> | < Identifier > < digito >
-    #< letra > ::= a | b | c | d | e | f | g | h | i | j | k | l | m | n | o | p | q | r | s | t | u | v | w | x | y | z
-
-
+# < Function > ::= < letra > | < Identifier > <letra> | < Identifier > < digito >
+# < letra > ::= a | b | c | d | e | f | g | h | i | j | k | l | m | n | o | p | q | r | s | t | u | v | w | x | y | z
 
 
 # def t_Bool(t):
@@ -183,19 +177,18 @@ t_function = r'[a-z]\s|[a-z_][a-z_0-9]*([0-9]|[a-z])'
 # Ignored characters
 t_ignore = " \t"
 
+
 # Error handling rule
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
-    
+
     t.lexer.skip(1)
-
-
 
 
 def getlinha(pos):
     tamlinha = 30
     pos = int(pos)
-    pos = math.floor(pos/tamlinha)
+    pos = math.floor(pos / tamlinha)
     return pos
 
 
@@ -204,7 +197,6 @@ lexer = lex.lex()
 tabelaSimbolos = []
 tabelaSimboloscount = 600
 data = " Int batata = 3.5 float bb = 12.21e3 a_0 'a' Program If ( ) Bool Bool == \"stringtest\" + - = If ( -5 ) Bool Bool == + - = If (  ) Bool Bool == + - = While Int"
-
 
 lexer.input(data)
 
@@ -216,33 +208,34 @@ Rafael Bessa Loureiro	xxx	xxx\n
 Pedro De Carvalho Marcelo	xxxxx	xxx\n
 Neilton Melgaço Lisboa Junior	xxx	xxx\n
 '''
-SaidaLex = Cabeçalho + "\n" + "tipo, Elemento lexico , Codigo , indicie tabela simb , linha  \n" 
+SaidaLex = Cabeçalho + "\n" + "tipo, Elemento lexico , Codigo , indicie tabela simb , linha  \n"
 
 # Tokenize
 while True:
     tok = lexer.token()
-    #tok lexpos, lineno,type,value
-    if not tok: #quando tok fica vazio 
-        break      # No more input
+    # tok lexpos, lineno,type,value
+    if not tok:  # quando tok fica vazio
+        break  # No more input
 
     if tok.type in Dic_Reserved_Atom_Cod:
-        SaidaLex += tok.type + " , " + tok.value + " , " +  Dic_Reserved_Atom_Cod[tok.type] + " , " + "none"+" , "+  str(getlinha(tok.lexpos)) +"\n"
-    
+        SaidaLex += tok.type + " , " + tok.value + " , " + Dic_Reserved_Atom_Cod[
+            tok.type] + " , " + "none" + " , " + str(getlinha(tok.lexpos)) + "\n"
+
     elif tok.type in Dic_not_Reserved_Atom:
-        SaidaLex += tok.type + " , " +tok.value + " , " +  "none" + " , " + str(tabelaSimboloscount) +" , "+  str(getlinha(tok.lexpos)) +"\n"
+        SaidaLex += tok.type + " , " + tok.value + " , " + "none" + " , " + str(tabelaSimboloscount) + " , " + str(
+            getlinha(tok.lexpos)) + "\n"
         tabelaSimbolos.append({
             "tipo": str(tok.type),
-            "value":str(tok.value),
-            "indice":str(tabelaSimboloscount)
+            "value": str(tok.value),
+            "indice": str(tabelaSimboloscount)
         })
         tabelaSimboloscount += 1
-        
-    else:
-        pass #caso n seja um atomo reservado, puxa a tabela de simbolos
 
-    
-    print(tok) # saida = token, simbolo, linha,coluna
-                #linha tem que ser feita uma função especial
+    else:
+        pass  # caso n seja um atomo reservado, puxa a tabela de simbolos
+
+    print(tok)  # saida = token, simbolo, linha,coluna
+    # linha tem que ser feita uma função especial
 
 f = open("meuteste.LEX", "w")
 f.write(SaidaLex)
